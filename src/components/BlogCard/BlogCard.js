da3edea331e3419afea132157d309d63e2988ef9
TiesHogenboom <70697022+TiesHogenboom@users.noreply.github.com>
2023-03-07 18:42:54 +0100

    worked on rightpane & blogcard, still fixing layout

--- src/components/BlogCard/BlogCard.js
+++ src/components/BlogCard/BlogCard.js
@@ -4,7 +4,23 @@ import "./BlogCard.css"
 const BlogCard = () => {
     return(
         <>
-    <article className="BlogCard"><h1>card</h1></article>
+    <article className="BlogCard">
+        <ul className="BlogCard__list">
+            <li>
+                <a href="/music">
+                    <img className="BlogCard__img"src="https://f4.bcbits.com/img/a1923504110_2.jpg" alt=""></img>
+                    <h6 className="BlogCard__title">Title</h6>
+                </a>
+            </li>
+            <li>
+                <a href="/music">
+                    <img className="BlogCard__img"src="https://f4.bcbits.com/img/a1923504110_2.jpg" alt=""></img>
+                    <h6 className="BlogCard__title">Title</h6>
+                </a>
+            </li>
+        </ul>
+        
+        </article>
     </>
         );
 }
